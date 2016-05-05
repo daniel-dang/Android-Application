6b6100ab38d84a4ba36cdded2dec090eecd3ae2d
Daniel Dang <manhdang132@yahoo.com>
2016-05-05 16:05:49 -0500

    Modification to android files [adding comments, and change hex conversion method]

--- FirstApp/NumberBaseConverter/src/main/java/com/example/hoangdang/firstapp/BinaryProcess.java
+++ FirstApp/NumberBaseConverter/src/main/java/com/example/hoangdang/firstapp/BinaryProcess.java
@@ -1,32 +1,50 @@
 package com.example.hoangdang.firstapp;
 
+//import stack and array list package
 import java.util.Arrays;
 import java.util.Stack;
 
+//class Binary Process
 public class BinaryProcess {
     private int input;
 
+    //Constructor - take in an int to initialize the input variable
     public BinaryProcess(int input){
         this.input = input;
     }
 
+    //Class accessor
     public String getInput(){
         return Integer.toString(input);
     }
 
+    /*
+    Method convert decimal to binary number.
+    This method take the class's variable "input" mod 2 to get remainder
+    Then the new divider will be "input" / 2
+    repeat process until divider is 0
+    This method use a stack to push result of mod to the end of the stack.
+    As we know, binary result from dividing by 2 goes backward, that's why we
+    use stack
+    */
     public String convertBin(){
         Stack binArr = new Stack();
         int result = input;
         int remain;
         int count = 0;
+
+        //This loop calculate binary by divide by 2
         while(result != 0) {
             remain = result % 2;
             result = result / 2;
-            binArr.push(remain);
+            binArr.push(remain);    //push remainder to stack
             count++;
         }
 
         String temp = "";
+        //pop stack from top to bottom. Stacks are LIFO.
+        //After we push until stack empty, add those value and convert them to a string.
+        //add all string value together in a loop to make a long string.
         while(!binArr.empty()){
             temp += binArr.pop().toString();
         }
@@ -34,6 +52,15 @@ public class BinaryProcess {
     }
 
 
+    /*
+    Method convert decimal to hexadecimal number.
+    This method take the class's variable "input" mod 16 to get remainder
+    Then the new divider will be "input" / 16
+    repeat process until divider is 0
+    This method use a stack to push result of mod to the end of the stack.
+    If the remainder is from 10-15 we push a letter corresponding to the value of hexadecimal,
+    otherwise, push number inthe stack normally if remainder is from 0-9
+    */
     public String convertHex(){
         Stack hexArr = new Stack();
         int result = input;
@@ -42,7 +69,23 @@ public class BinaryProcess {
         while(result != 0){
             remain = result % 16;
             result = result / 16;
-            hexArr.push(remain);
+
+            //If remainder fall from range 10-15 push letter into stack accordingly
+            if (remain == 10)
+                hexArr.push("A");
+            else if(remain == 11)
+                hexArr.push("B");
+            else if(remain == 12)
+                hexArr.push("C");
+            else if(remain == 13)
+                hexArr.push("D");
+            else if(remain == 14)
+                hexArr.push("E");
+            else if(remain == 15)
+                hexArr.push("F");
+            else
+                hexArr.push(remain);    //else push the number in normally
+
             count++;
         }
 
@@ -53,6 +96,13 @@ public class BinaryProcess {
         return temp;
     }
 
+    /*
+    Method convert decimal to octal number.
+    This method take the class's variable "input" mod 8 to get remainder
+    Then the new divider will be "input" / 8
+    repeat process until divider is 0
+    This method use a stack to push result of mod to the end of the stack.
+    */
     public String convertOcta(){
         Stack octaArr = new Stack();
         int result = input;
--- FirstApp/NumberBaseConverter/src/main/java/com/example/hoangdang/firstapp/MainActivity.java
+++ FirstApp/NumberBaseConverter/src/main/java/com/example/hoangdang/firstapp/MainActivity.java
@@ -15,12 +15,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-        private EditText input;
-        private TextView binResult;
-        private TextView hexResult;
-        private TextView octaResult;
-        private Button button;
+        private EditText input;         //This is the input box to get user's input
+        private TextView binResult;     //This is the binary conversion result box
+        private TextView hexResult;     //This is the hexadecimal conversion result box
+        private TextView octaResult;    //This is the octaldecimal conversion result box
+        private Button button;          //This is the convert button.
 
+    //At onCreate find all view and put them into class variable.
+    //Method used (ViewType)findViewByID(location)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,11 +30,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        button = (Button)findViewById(R.id.button);
-        input = (EditText)findViewById(R.id.decInput);
-        binResult = (TextView) findViewById(R.id.binResult);
-        hexResult = (TextView) findViewById(R.id.hexResult);
-        octaResult = (TextView) findViewById(R.id.octaResult);
+        //NOTE**: it is recommended that the private variable is the same name
+        //as the id we set for the view.
+        button = (Button)findViewById(R.id.button);             //Find button view
+        input = (EditText)findViewById(R.id.decInput);          //Find user input box view
+        binResult = (TextView) findViewById(R.id.binResult);    //Find binary result text field view
+        hexResult = (TextView) findViewById(R.id.hexResult);    //Find hexadecimal result text field view
+        octaResult = (TextView) findViewById(R.id.octaResult);  //Find octaldecimal result text field view
     }
 
     @Override
@@ -40,19 +44,26 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
     }
 
+    /*
+        onResume we get the input and process the input by calling method from the BinaryProcess class.
+     */
     @Override
     protected void onResume() {
         super.onResume();
 
+        //Make a listen event and listen to process when the user click submit button.
         button.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View view) {
+                        //get input from user. Input is in EditedText type so we convert it to string.
                         int inp = Integer.parseInt(input.getText().toString());
-                        BinaryProcess bin = new BinaryProcess(inp);
-                        String binaryResult = bin.convertBin();
-                        String hexadecimalResult = bin.convertHex();
-                        String octadecimalResult = bin.convertOcta();
 
+                        BinaryProcess bin = new BinaryProcess(inp);     //new instance of BinaryProcess class
+                        String binaryResult = bin.convertBin();         //convert to binary
+                        String hexadecimalResult = bin.convertHex();    //convert to hexadecimal
+                        String octadecimalResult = bin.convertOcta();   //convert to octaldecimal
+
+                        //Set three result text field calculated result.
                         binResult.setText(binaryResult);
                         hexResult.setText(hexadecimalResult);
                         octaResult.setText(octadecimalResult);
